d978cc048d1928f3573d7a1f52d8c16f6e2b4450
Tanzeel Khan <ktanzeel80@gmail.com>
2021-05-30 00:49:11 +0530

    Now deploying

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np 
 import flask
 import pickle
+import joblib
 from flask import Flask, render_template, request
 app=Flask(__name__)
 @app.route("/")
@@ -16,17 +17,28 @@ def ValuePredictor(to_predict_list):
 @app.route("/predict",methods = ["POST"])
 def result():
  if request.method == "POST":
-   perc = request.form.get('perc')
-   age = request.form.get('age')
-   income = request.form.get('income')
-   c1 = request.form.get('c1')
-   c2 = request.form.get('c2')
-   c3 = request.form.get('c3')
-   auc = request.form.get('auc')
-   num = request.form.get('num')
-   s_channel = request.form.get('s_channel')
-   residence = request.form.get('residence')
-   prediction = [perc, age, income, c1, c2, c3, auc, num, s_channel, residence]
- return render_template("predict.html",prediction=prediction)
+   idd = int(request.form.get('idd'))
+   perc = float(request.form.get('perc'))
+   age = int(request.form.get('age'))
+   income = int(request.form.get('income'))
+   c1 = float(request.form.get('count1'))
+   c2 = float(request.form.get('count2'))
+   c3 = float(request.form.get('count3'))
+   auc = float(request.form.get('auc'))
+   num = int(request.form.get('num'))
+   s_channel = list(request.form.get('s_channel'))
+   residence = list(request.form.get('residence'))
+   age_group = list(request.form.get('age_group'))
+   status = list(request.form.get('status'))
+   prediction = [idd, perc, age, income, c1, c2, c3, auc, num] + s_channel + residence + age_group + status
+   try:
+      prediction = np.array(prediction)
+      prediction = prediction.reshape(1, -1)    
+      file = open("model.pkl","rb")
+      trained_model = joblib.load(file)
+      result = trained_model.predict(prediction)
+      return str(result)
+   except Exception:
+      return Exception.msg()
 if __name__ == "__main__":
  app.run(debug=True)
