ab5e3c65f78adcabd45df9c68bfad07a72cb805a
Tanzeel Khan <ktanzeel80@gmail.com>
2021-05-29 19:33:22 +0530

    Last touches

--- app.py
+++ app.py
@@ -5,22 +5,22 @@ import flask
 import pickle
 from flask import Flask, render_template, request
 app=Flask(__name__)
-@app.route(‘/’)
+@app.route("/")
 def index():
- return flask.render_template(‘index.html’)
+ return flask.render_template("index.html")
 def ValuePredictor(to_predict_list):
  to_predict = np.array(to_predict_list).reshape(1,4)
- loaded_model = pickle.load(open(“model.pkl”,”rb”))
+ loaded_model = pickle.load(open("model.pkl","rb"))
  result = loaded_model.predict(to_predict)
  return result[0]
-@app.route(‘/predict’,methods = [‘POST’])
+@app.route("/predict",methods = ["POST"])
 def result():
- if request.method == ‘POST’:
- to_predict_list = request.form.to_dict()
- to_predict_list=list(to_predict_list.values())
- to_predict_list = list(map(float, to_predict_list))
- result = ValuePredictor(to_predict_list)
- prediction = str(result)
- return render_template(“predict.html”,prediction=prediction)
-if __name__ == “__main__”:
+ if request.method == "POST":
+    to_predict_list = request.form.to_dict()
+    to_predict_list=list(to_predict_list.values())
+    to_predict_list = list(map(float, to_predict_list))
+    result = ValuePredictor(to_predict_list)
+    prediction = str(result)
+ return render_template("predict.html",prediction=prediction)
+if __name__ == "__main__":
  app.run(debug=True)
