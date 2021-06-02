72c1111932e817d57e755faed421fc6d57a18354
Tanzeel Khan <ktanzeel80@gmail.com>
2021-06-02 13:23:48 +0530

    Fixing Bugs

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import joblib
 from flask import Flask, render_template, request
 import requests
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
